fd67a3174567c16716f137f2b1f96ce43f0dd83d
Dmytro S. <dp270788sdo@gmail.com>
2026-04-19 15:42:37 -0230

    ver 1.2, #

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+class Storage {
+   #items;
+  constructor(items) {
+    this.#items = items;
+     }
+    getItems() {
+      return this.#items;
+    }
+    removeItem(itemToRemove) {
+      this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+    addItem(itemToAdd) {
+      this.#items.push(itemToAdd);
+    }
+ 
+
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
